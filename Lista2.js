8c19353c74e63f5410f661994e530cb41bb74692
Gerson <gersoncastrogomes95@gmail.com>
2024-09-16 17:29:24 -0300

    Terminando Questões

--- Lista2.js
+++ Lista2.js
@@ -158,32 +158,32 @@ const prompt = require('prompt-sync')()
 // nome, nota1, e nota2. Use for of para calcular a média das notas de cada
 // aluno e exibir o nome do aluno junto com sua média.
 
-    // class aluno {
-    //     constructor(nome, nota1, nota2, nota3) {
-    //         this.nome = nome
-    //         this.nota1 = nota1
-    //         this.nota2 = nota2
-    //         this.nota3 = nota3
+// class aluno {
+//     constructor(nome, nota1, nota2, nota3) {
+//         this.nome = nome
+//         this.nota1 = nota1
+//         this.nota2 = nota2
+//         this.nota3 = nota3
 
-    //     }
-    //     media() {
-    //         let sum = this.nota1 + this.nota2 + this.nota3
-    //         let avg = sum / 3
-    //         return avg.toFixed(2)
-    //     }
+//     }
+//     media() {
+//         let sum = this.nota1 + this.nota2 + this.nota3
+//         let avg = sum / 3
+//         return avg.toFixed(2)
+//     }
 
-    // }
+// }
 
-    // let aluno1 = new aluno('Gerson', 7, 7, 8.9)
-    // let aluno2 = new aluno('Alzira', 5, 6.5, 9)
-    // let aluno3 = new aluno('Roberto', 7, 3, 9.9)
-    // let aluno4 = new aluno('Augusto', 5, 7.4, 8.5)
-    // let array = [aluno1, aluno2, aluno3, aluno4]
-    // for (const element of array) {
-    //     console.log(`${element.nome}: ${element.media()}`);
-        
-        
-    // }
+// let aluno1 = new aluno('Gerson', 7, 7, 8.9)
+// let aluno2 = new aluno('Alzira', 5, 6.5, 9)
+// let aluno3 = new aluno('Roberto', 7, 3, 9.9)
+// let aluno4 = new aluno('Augusto', 5, 7.4, 8.5)
+// let array = [aluno1, aluno2, aluno3, aluno4]
+// for (const element of array) {
+//     console.log(`${element.nome}: ${element.media()}`);
+
+
+// }
 
 //     6. Filtrando Arrays de Objetos
 // ○ Objetivo: Crie um array de objetos funcionarios, onde cada objeto
@@ -191,3 +191,86 @@ const prompt = require('prompt-sync')()
 // e exibir apenas os funcionários cujo salário seja maior que um valor
 // específico.
 
+// class funcionario {
+//     constructor(nome, cargo, salario) {
+//         this.nome = nome
+//         this.cargo = cargo
+//         this.salario = salario
+//     }
+
+// }
+// function valorDeCorte(array, valor) {
+//     for (const element of array) {
+//         if(element.salario > valor){
+//             console.log(element);
+
+//         }
+
+//     }
+// }
+// let funcionario1 = new funcionario('Carlos', 'Caixa', 3200)
+// let funcionario2 = new funcionario('Maria', 'Desenvolvedora', 5100)
+// let funcionario3 = new funcionario('Renata', 'Diretora', 8300)
+// let array = [funcionario1, funcionario2, funcionario3]
+// valorDeCorte(array,4000)
+
+
+// 7. Modificando Objetos em um Array
+// ○ Objetivo: Crie um array de objetos produtos, onde cada objeto tem nome,
+// preco, e desconto. Use forEach para aplicar um desconto de 10% em todos
+// os produtos e exibir o novo preço.
+
+
+// class produtos {
+//     constructor(nome, preco, desconto) {
+//         this.nome = nome
+//         this.preco = preco
+//         this.desconto = desconto
+
+//     }
+
+// }
+// let produtos1 = new produtos('Arroz', 10, 0)
+// let produtos2 = new produtos('Feijão', 8.50, 0)
+// let produtos3 = new produtos('Macarrão', 5.40, 0)
+// let array = [produtos1, produtos2, produtos3]
+
+// array.forEach(element => {
+//     element.desconto = 10
+//     element.precoReal = element.preco * (1 - (element.desconto / 100))
+//     console.log(`${element.nome}: R$${element.precoReal.toFixed(2)}`);
+
+
+// })
+
+
+
+// 8. Criando Novos Arrays a Partir de Objetos
+// ○ Objetivo: Crie um array de objetos filmes, onde cada filme tem titulo,
+// diretor, e anoLancamento. Use forEach para criar um novo array
+// contendo apenas os títulos dos filmes.
+
+// class filmes {
+//     constructor(titulo, diretor, anoLancamento) {
+//         this.titulo = titulo
+//         this.diretor = diretor
+//         this.anoLancamento = anoLancamento
+//     }
+// }
+
+// let filme1 = new filmes('Resident Evil', 'Scorcese', '1995')
+// let filme2 = new filmes('Spider-man', 'Bay', '1990')
+// let array = [filme1, filme2]
+// let arrayTitulo = []
+// array.forEach(element => {
+    
+//     arrayTitulo.push(element.titulo)
+// });
+// console.log(arrayTitulo);
+
+
+
+// 9. Contabilizando Elementos com uma Condição
+// ○ Objetivo: Crie um array de objetos clientes, cada um com propriedades
+// nome, idade, e cidade. Use forEach para contar quantos clientes têm mais
+// de 30 anos.
